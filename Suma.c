e4097482827f71027947666a0d7a620c1dfa9a71
nadiaka <36199838+nadiaka@users.noreply.github.com>
2018-02-12 09:01:38 +0100

    Create Suma.c
    Dodawanie liczb w C

---
+++ Suma.c
@@ -0,0 +1,13 @@
+// Dodawanie dwóch liczb w C
+
+#include <stdio.h>
+
+int main() {
+   int x;
+   scanf("%d", &x);
+   int y;
+   scanf("%d", &y);
+   int suma;
+   suma = x+y;
+   printf("%d + %d = %d", x, y, suma);
+}
